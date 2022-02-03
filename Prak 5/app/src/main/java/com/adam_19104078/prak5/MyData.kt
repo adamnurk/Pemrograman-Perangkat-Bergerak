d19c1e64ab93e5c8753d50d75016b043bbf09199
Adam <72558520+adamnurk@users.noreply.github.com>
2022-02-03 14:14:50 +0700

    prak5/modul6

---
+++ Prak 5/app/src/main/java/com/adam_19104078/prak5/MyData.kt
@@ -0,0 +1,7 @@
+package com.adam_19104078.prak5
+
+class MyData (
+    var name: String,
+    var description: String,
+    var photo: String
+)
---
+++ Prak 5/app/src/main/java/com/adam_19104078/prak5/adapter/ListMyDataAdapter.kt
@@ -0,0 +1,47 @@
+package com.adam_19104078.prak5.adapter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.adam_19104078.prak5.MyData
+import com.adam_19104078.prak5.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+class ListMyDataAdapter(private val listMyData: ArrayList<MyData>) :
+    RecyclerView.Adapter<ListMyDataAdapter.ListViewHolder>() {
+    private lateinit var tv_item_name : TextView
+    private lateinit var tv_item_description : TextView
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(listMyData[position])
+    }
+
+    override fun getItemCount(): Int = listMyData.size
+
+    inner class ListViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        fun bind(myData: MyData) {
+            with(itemView) {
+                Glide.with(itemView.context)
+                    .load(myData.photo)
+                    .apply(RequestOptions().override(55, 55))
+                    .into(findViewById(R.id.img_item_photo))
+                tv_item_name = findViewById(R.id.tv_item_name)
+                tv_item_description = findViewById(R.id.tv_item_description)
+                tv_item_name.text = myData.name
+                tv_item_description.text = myData.description
+            }
+        }
+    }
+}
